45a792bff82026872e1768389216594db16ec3fb
amadden80 <amadden80@gmail.com>
2014-04-18 14:57:36 -0400

    Color for string in MTA

--- exercises/ex_1395780192/solutions/mta-solution.rb
+++ exercises/ex_1395780192/solutions/mta-solution.rb
@@ -1,6 +1,12 @@
 require 'pry'
 require 'rainbow'
 
+class String
+  def color(hex)
+    Rainbow(self).color(hex)
+  end
+end
+
 n = ['ts', '34th', '28th-n', '23rd-n', 'us']
 l = ['8th', '6th', 'us', '3rd', '1st']
 s = ['gc', '33rd', '28th-s', '23rd-s', 'us']
